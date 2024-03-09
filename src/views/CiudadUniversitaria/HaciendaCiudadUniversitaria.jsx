edaac84eb503e6eb58180495d031263e21ab7521
CChirino <Christopherchirinosj@gmail.com>
2024-03-09 10:37:57 -0400

    Cambios diferencia p y title

--- src/views/CiudadUniversitaria/HaciendaCiudadUniversitaria.jsx
+++ src/views/CiudadUniversitaria/HaciendaCiudadUniversitaria.jsx
@@ -22,7 +22,7 @@ const HaciendaCiudadUniversitaria = () => {
                     <p className="pt-1 pb-3">
                     Desde la época colonial los terrenos hoy de la CUC eran destinados al cultivo de caña de azúcar y la destilación del ron, eran propiedad de la familia Ibarra. En la casona de la hacienda se alojo Alexander Humboldt durante su paso por Caracas, fue el sitio donde Simón Bolívar se hospedo junto a José María Vargas en la ultima visita a la ciudad del primero, y juntos en sus salones redactaron el decreto para la fundación de la Universidad Central de Venezuela, institución a la cual hoy pertenece el campus.
                     </p>
-                    <p className="pt-1 pb-3">
+                    <p className="pt-1 pb-24">
                     En 1943 el presidente Medina Angarita, compraría los terrenos de la hacienda y los donaría a la Universidad para comenzar así el proceso de urbanización y construcción de la Ciudad Universitaria, con la decisión también de conservar la casona como parte de la institución por su valor histórico y arquitectónico.
                     </p>
                  </div>
--- src/views/CiudadUniversitaria/HospitalCiudadUniversitaria.jsx
+++ src/views/CiudadUniversitaria/HospitalCiudadUniversitaria.jsx
@@ -13,12 +13,12 @@ const HospitalCiudadUniversitaria = () => {
             </div>
             <div className="w-3/5 pt-5 text-white pt-6 text-left pr-32 ml-24">
                  <div className="container">
-                    <h3 className="pb-10 subtitle-section">Un nuevo hospital para una Caracas Moderna
+                    <h3 className="pb-16 subtitle-section">Un nuevo hospital para una Caracas Moderna
                     </h3>
                     <p className="pt-1 pb-3">
                     Durante los años cuarenta la insuficiencia del Hospital Vargas promueve la realización de un proyecto a través del Ministerio de Sanidad, y se vincula a la necesidad de práctica de los estudios de medicina. Esta fue la necesidad de partida y la excusa que necesito el ejecutivo nacional, al momento el palacio donde se ubicaba la Universidad Central de Venezuela no era suficiente ni moderno.
                     </p>
-                    <p className="pt-1 pb-3">
+                    <p className="pt-1 pb-24">
                     La primera propuesta urbana general partía con el hospital en el extremo occidental del campus, mientras que un gran eje sentido este-oeste organizaba las edificaciones transversales o paralelas al mismo, todo inmerso en una simetría impoluta con un remate en un estadio olímpico en el extremo oriental. Un diseño completamente racionalista y basado en su aprendizaje en Bellas Artes. De este primer momento pre-moderno surgirían los primeros edificios como el propio hospital, los institutos de Medicina Experimental, el Anatómico y Anatomopatológico.
                     </p>
                  </div>
