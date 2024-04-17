5b50736b93787afba2e6910fd6d891b7837bfcc2
CChirino <Christopherchirinosj@gmail.com>
2024-04-17 19:00:51 -0400

    Paisaje textos!

--- src/views/Paisaje/PaisajeInformacion.jsx
+++ src/views/Paisaje/PaisajeInformacion.jsx
@@ -20,7 +20,10 @@ const PaisajeInformacion = () => {
                     En sus primeros trazos el campus ubicaba las edificaciones sobre un espacio verde central a todo lo largo, con anillos de jardines y caminerías que rodeaban externamente. Al ir avanzando en el desarrollo urbano, el paisaje de la misma se vio alterado distanciándose completamente de las ideas originales.
                     </p>
                     <p className="pt-1 pb-11">
-                    Actualmente el paisaje está incorporado al diseño del campus. Lo construido y lo natural se entrecruzarían en distintos juegos compositivos como patios, vistas panorámicas de jardines, escenografías de fondo.
+                    “…se puede afirmar que tanto el paisajismo, la arquitectura y las obras de arte tenían para Villanueva el mismo valor y orden de importancia en este complejo urbano; el paisajismo constituye la puesta en escena de la obra arquitectónica en fusión con la obra artística.” 
+                    </p>
+                    <p className="pt-1 pb-11">
+                    Villanueva umbral de un descubrimiento paisajista. Aguedita Coss, 2011
                     </p>
                  </div>
             </div>
@@ -37,10 +40,30 @@ const PaisajeInformacion = () => {
                  <div className="container">
                     <h2 className=" pb-7 subtitle-section">Del patio de jardín colonial al bosque tropical</h2>
                     <p className="pt-1 pb-1">
-                    Villanueva buscó como desdibujar la relación entre el adentro y el afuera, borrar los límites entre edificios y entorno, como en la facultad de humanidades donde un bosque es el telón de fondo para los salones de aulas que se abren totalmente al exterior. En otras instancias el paisajismo ingresa a múltiples edificaciones donde se incorporan patios con vegetación influyendo la composición espacial, de forma similar a las antiguas casas coloniales venezolanas, como sucede en el instituto botánico o la facultad de farmacia.                     
+                    Siendo el Hospital Clínico el primer edificio en diseñarse de todo el conjunto de la Ciudad Universitaria de Caracas, es muy distintivo señalar que el arquitecto configuro un gran vacío al centro. Un patio como aquellos de las viviendas coloniales, una relación muy directa entre el volumen y este espacio. En contra parte, los 8 brazos de pabellones de habitaciones tienen un pasillo perimetral, que permite a los usuarios integrarse tímidamente a su entorno.              
                     </p>
-                    <p className="pt-1 pb-20">
-                    La decisión de crear corredores cubiertos y espacios intersticiales ayudó a configurar una mejor relación con el entorno, pero esto no freno a Villanueva de crear momentos más contemplativos y pictóricos como la tierra de nadie, el bosque de los estudiantes o el Palmetum.
+                    <p className="pt-1 pb-1">
+                    Los primeros ejercicios formales de paisajismo de Villanueva en el campus, son los jardines contenidos en dos o más de sus bordes, como sería el caso de los jardines y plantíos de la antigua Escuela Técnica Industrial, así como el jardín que se forma entre los institutos Anatómicos y de Medicina Experimental. Estos espacios, verdes, tenían una tímida integración con los edificios, que les servían de borde.        
+                    </p>
+                    <p className="pt-1 pb-1">
+                    A medida que progresaba el diseño y construcción de la Ciudad Universitaria la relación con el paisajismo y su integración iría cambiando, a medida que el arquitecto buscaba nuevas formas. El uso en diversas ocasiones de patios de luz y vegetación, que inicio en Plaza Cubierta y repetiría en otras oportunidades, estos puntos de luz y color dan una nueva vida a los espacios interiores de los edificios.
+                    </p>
+                    <p className="pt-1 pb-1">
+                    A lo largo de sus 3 décadas de trabajo Villanueva buscó como desdibujar la relación entre el adentro y el afuera, borrar los límites entre edificios y entorno. Por ejemplo, el patio interior junto a la cafetería de la Facultad de Arquitectura y Urbanismo, es un precioso espacio y telón de fondo para quienes hacen vida en la facultad.
+                    </p>
+                    <p className="pt-1 pb-1">
+                    Entre sus prácticas se dio la tarea de crear una sala hipóstila de chaguaramos, una suerte de plaza cubierta natural y un sector de bosque que permitía distanciar las antiguas residencias de las zonas más académicas del campus.
+                    </p>
+                    <p className="pt-1 pb-1">
+                    La facultad de humanidades sería un campo de experimentación por su conformación volumétrica, al rodear el volumen de jardines permitió abrir los salones en el nivel inferior, con ventanales de piso a techo que permiten una total integración entre ambos componentes.                     </p>
+                    <p className="pt-1 pb-1">
+                    En otras instancias el paisajismo se incorpora a la composición, superando el patio de luz buscando que la vegetación ingrese dentro de los edificios. Esto lo ensayaría tanto en el Instituto botánico como en la biblioteca de la escuela de psicología.
+                    </p>
+                    <p className="pt-1 pb-1">
+                    La decisión de crear corredores cubiertos y espacios intersticiales ayudó a configurar una mejor relación con el entorno, pero esto no freno a Villanueva de crear momentos más contemplativos y pictóricos como la tierra de nadie, el bosque de los estudiantes o el Palmetum. 
+                    </p>
+                    <p className="italic pt-1 pb-20">
+                    “He aquí un ejemplo de verdadera síntesis plástica lograda con material nuevo, un material viviente y transitorio como la vegetación, en función pública, es decir, social, y atributos seguramente pictóricos y escultóricos. Es difícil decir hasta qué punto los jardines y parques de Burle Marx son auténtica arquitectura. En todo caso hay que estar de acuerdo que son espacios. Espacios a veces en función de la arquitectura y a veces el mismo espacio cerrado: no con muros y techos naturalmente, sino con verde, con árboles y flores, agua y piedras, y más que espacio cerrado se trata de espacio compuesto, ordenado con una intención de secuencia visual o de conformación sicológica. Para quien haya visto los bocetos del paisajista brasileño no cabe duda de que este trabaja además como un pintor o un escultor. La determinación de los colores y la selección de las esencias están en función de un diseño preconcebido que es en sí una obra pictórica. En función de la arquitectura-arquitectura, pintura y escultura ellos mismos-, los parques de Burle Marx constituyen una forma de renovar, con vocabulario nuevo, el antiguo lenguaje de la síntesis de las artes”. Carlos Raúl Villanueva sobre Burle Marx y paisajismo 
                     </p>
                  </div>
             </div>
@@ -57,10 +80,27 @@ const PaisajeInformacion = () => {
                  <div className="container">
                     <h2 className="subtitle-section">Jardín Botánico</h2>
                     <p className="pt-1 pb-1">
-                    Por su ubicación dentro de la ciudad, el limite norte de la CUC sería una zona vegetal protectora, formado sobre terrenos planos aluvionales recientes y colinas adyacentes de esquistos y calizas, la institución fue fundada por el botánico Dr. Tobias Lasser, interesado en recopilar y preservar especies vegetales de todo el territorio nacional, así como del trópico. El Jardín Botánico ocupa una superficie total de 63.01 hectáreas, repartidas en 13.81 ha. planas y 49.2 ha. de colinas.
+                    Por su ubicación dentro de la ciudad, el limite norte de la CUC sería una zona vegetal protectora, formado sobre terrenos planos aluvionales recientes y colinas adyacentes de esquistos y calizas, la institución fue fundada por el botánico Dr. Tobias Lasser, interesado en recopilar y preservar especies vegetales de todo el territorio nacional, así como del trópico. El Jardín Botánico ocupa una superficie total de 63.01 hectáreas, repartidas en 10 ha planas y 60 ha de colinas.
+                    </p>
+                    <p className="pt-1 pb-1">
+                    En 1945 fue fundado con el nombre de Jardín Botánico de la Universidad Central de Venezuela. Como resultado de la propuesta realizada en 1944 por Tobías Lasser y Armando Vegas, al Consejo Directivo del Instituto Ciudad Universitaria (ICU). Fue su primer director Tobías Lasser, desde su fundación hasta 1981. El diseño general de plantíos dentro del jardín es obra del Dr. Lasser, reproduciendo la mayoría de ellas en los espacios del Jardín. Contiene un total de 2.500 especies vegetales y en el hacen vida alrededor de 2.000 especies animales.
+                    </p>
+                    <p className="pt-1 pb-1">
+                    El Jardín Botánico de Caracas se ideó, para ser un centro de investigación y de enseñanza botánica, con colecciones de plantas nativas y exóticas para el conocimiento, disfrute y esparcimiento de los habitantes de la ciudad.
                     </p>
                     <p className="pt-1 pb-20">
-                    El diseño general de plantíos dentro del jardín es obra del Dr. Lasser, reproduciendo la mayoría de ellas en los espacios del Jardín. Contiene un total de 2.500 especies vegetales y en el hacen vida alrededor de 2.000 especies animales.
+                    Como otras entidades de este tipo, contiene varias zonas de interés:
+                    <ol>
+                      <li>Lagunas donde observar especies acuáticas como Nenúfares, lotos, lirios y juncos.</li>
+                      <li>Jardín Xerofítico.</li>
+                      <li>Palmetum</li>
+                      <li>Arboterum (ocupa alrededor de 7 hectáreas planas del jardín)</li>
+                      <li>Orquidáceas</li>
+                      <li>Bromeliáceas</li>
+                      <li>Aráceas</li>
+                      <li>Zingibirales</li>
+                      <li>Jardín Etnobotánico</li>
+                    </ol>
                     </p>
                  </div>
             </div>
