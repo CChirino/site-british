2991703a78ab3c28e0cc4839fd5320bcc5dc8a7a
CChirino <Christopherchirinosj@gmail.com>
2024-05-28 20:20:09 -0400

    Home edit

--- src/views/FirstView.jsx
+++ src/views/FirstView.jsx
@@ -1,45 +1,26 @@
 import React from "react";
-import { useSpring, animated } from "@react-spring/web";
 import Header from "../components/Header";
 import { useTranslation } from 'react-i18next';
 
 const FirstView = () => {
-  const [isImageHovered, setIsImageHovered] = React.useState(false);
   const { i18n } = useTranslation();
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
 
-  const handleMouseEnter = () => {
-    setIsImageHovered(true); // Establecer isImageHovered en true cuando el mouse entra en la imagen
-  };
-
-  const handleMouseLeave = () => {
-    setIsImageHovered(false); // Establecer isImageHovered en false cuando el mouse sale de la imagen
-  };
-
-  const zoomAnimation = useSpring({
-    transform: `scale(${isImageHovered ? 1.2 : 1})`,
-  });
-
   return (
     <div className="bg-neutral-500 pb-10">
       <Header />
       <div className="flex flex-col items-center pt-5 pb-5">
-        <div
-          className="flex flex-col items-center pt-5 pb-5"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <animated.img
+        <div className="flex flex-col items-center pt-5 pb-5">
+          <img
             src="/static/images/mockup EVA-67.png"
             alt="nubes"
             className="clouds w-1/2 sm:w-1/3 md:w-1/7 lg:w-1/8"
-            style={zoomAnimation}
           />
         </div>
-        <div className="absolute end-1/3	 flex flex-col mt-10" style={{ marginRight: "16%", marginTop:"7%" }}>
+        <div className="absolute end-1/3 flex flex-col mt-10" style={{ marginRight: "16%", marginTop: "7%" }}>
           <p className="text-black px-4 py-2 mb-2" style={{ transform: "rotate(-45deg)" }} onClick={() => changeLanguage('es')}>Español</p>
           <p className="text-black px-4 py-2" style={{ transform: "rotate(-45deg)", marginRight: "15%" }} onClick={() => changeLanguage('en')}>English</p>
         </div>
