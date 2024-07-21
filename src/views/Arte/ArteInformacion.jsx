3e5e604aed9b205a72a6a1ef2c0cc39c37a268b9
CChirino <Christopherchirinosj@gmail.com>
2024-07-20 23:43:15 -0400

    Arte modificaciones

--- src/views/Arte/ArteInformacion.jsx
+++ src/views/Arte/ArteInformacion.jsx
@@ -12,12 +12,15 @@ const ArteInformacion = () => {
             </div>
             <div className="w-3/5 text-neutral-500 pt-12 text-left pr-16 ml-16">
                  <div className="container pb-10">
-                    <h2 className=" pb-7 subtitle-section">La colección “Síntesis de las Artes”</h2>
+                    <h2 className=" pb-7 subtitle-section">Proyecto “Síntesis de las Artes”</h2>
                     <p className="pt-1 pb-3">
-                    Para Villanueva la incorporación de las artes estaba considerada desde el primer momento que diseñaba y conceptualizaba los distintos espacios de la Ciudad Universitaria. Un total de 107 obras, que abarcan murales pictóricos, abstractos, figurativos, esculturas, vitrales y móviles conformar la colección. 
+                    El proyecto “Síntesis de las Artes” hizo de la Universidad Central de Venezuela un lugar para las artes, acaso el primero, por su ambición y completitud en la historia del país. Y el proyecto de la Ciudad Universitaria de Caracas de Villanueva fue el escenario de encuentro entre las artes con las diferentes tendencias, que entre los años 30 al 70 ocupó la imaginación de lo moderno en Venezuela, según el experto Luis Enrique Pérez Oramas.
+                    </p>
+                    <p className="pt-1 pb-3">
+                    Si bien bajo la mirada de Pérez Oramas la experiencia espacial junto a la vibración de la luz provocada por los quiebrasoles o brisolei son un efecto cinético, para Paulina Villanueva hija del maestro Villanueva, es una expresión de la Síntesis de las Artes, la experiencia del movimiento conjugado con la luz y el espacio de manera simultánea como consecuencia de las búsquedas desde la arquitectura.                    
                     </p>
                     <p className="pt-1 pb-11">
-                    Para incorporar las piezas de arte Villanueva se contactaba con los artistas que consideraba mejores para cada encargo, que imaginaba incorporar a los distintos espacios, a los internacionales le enviaba cartas con bocetos, planos y fachadas de los edificios, mientras que a los nacionales los invitaba a inspeccionar las obras.                    
+                    La variedad de artistas en cuanto a su origen también se traduce a una variedad de técnicas y formatos. Las piezas de arte pueden agruparse en figurativas aplicadas en murales y esculturas; las abstractas-geométricas de un grupo de artistas llamados disidentes aplicados en murales, bimurales, vitrales, esculturas y relieves; las policromías en las fachadas de los edificios y arte cinético.                    
                     </p>
                  </div>
             </div>
@@ -32,21 +35,36 @@ const ArteInformacion = () => {
             </div>
             <div className="w-3/5 text-neutral-500 pt-6 text-left pr-16 ml-16">
                  <div className="container pb-10">
-                    <h2 className="pb-7 subtitle-section">La pertinencia del arte</h2>
+                    <h2 className="pb-7 subtitle-section">Obras y autores</h2>
                     <p className="pt-1 pb-1">
-                    Cada una de estas obras tiene una razón de ser, su técnica y diseño están pensados conjuntamente entre Villanueva y el artista para dar un nuevo valor al espacio o el entorno; por ello tenemos murales a todo lo alto de fachadas de torres como en las facultades de arquitectura y farmacia, esculturas que son focos de atención en espacios públicos como el Pastor de nubes y el Amphion.
+                    Gracias a la formación del maestro Villanueva en la École de Beaux Arts y el conocimiento de primera mano sobre el arte y su actualidad, le facilitó la integración de muchos artistas plásticos venezolanos e internacionales activos en París, que coincidían en la búsqueda de lo moderno, y que estuvieron de acuerdo en trabajar conjuntamente con Villanueva.                    
                     </p>
                     <p className="pt-1 pb-1">
-                    En la arquitectura moderna, y más aún en el trópico, el trabajo de la luz es algo sumamente importante, por ello Villanueva quiso aprovechar la constante luz solar por medio de vitrales en momentos muy significativos de su obra, como es el Léger de la Biblioteca central.
+                    Villanueva, apasionado humanista, convocó a un total de 24 colaboradores provenientes de 9 países distintos, incluido Venezuela.                    
+                    </p>
+                    <p className="pt-1 pb-1">
+                    A los artistas internacionales les enviaba cartas con bocetos, planos y fachadas de los edificios, mientras que a los nacionales los invitaba a inspeccionar las obras en sitio de lo cual quedan ciertos registros. Son testimonio de ello las correspondencias con los artistas, los modelos y las fotografías en los talleres con las piezas de arte en proceso.                    
+                    </p>
+                    <p className="pt-1 pb-1">
+                    Las primeras colaboraciones aparecerían en 1951 en el Estadio Olímpico donde incorpora a artistas como: Francisco Narváez, Carlos González Bogen, Mateo Manaure y Armando Barrios.                    
+                    </p>
+                    <p className="pt-1 pb-1">
+                    En 1952, trabajaría en el Centro Directivo Cultural junto a artistas como Alexander Calder, Jean Arp, Henri Laurens, Antoine Pevsner, Fernand Léger, Víctor Vassarely, Mateo Manaure, Pascual Navarro, Miguel Arroyo, Héctor Poleo y Baltazar Lobo.                    
+                    </p>
+                    <p className="pt-1 pb-1">
+                    Durante la tercera etapa del campus1954 trabaja con Víctor Valera, Jean Arp y Sophie Tauber-Arp.                    
+                    </p>
+                    <p className="pt-1 pb-1">
+                    En 1956 una variedad de artistas trabajó con Villanueva en la Facultad de Arquitectura y Urbanismo; Alejandro Otero, Miguel Arroyo, Francisco Narváez, Alirio Oramas, Mateo Manaure, Víctor Valera, Jesús Soto y Alexander Calder.                    
                     </p>
                     <p className="pt-1 pb-20">
-                    Las piezas artísticas trascienden su rol, llegando en casos a ser las que definen el espacio más que las propias paredes o techos, este es el caso del Aula Magna y las Nubes de Calder que cumplen un doble rol escultórico y acústico. 
+                    Finalmente, en 1957 policromías de Omar Carreño en el edificio de Odontología y Alejandro Otero en Farmacia.                    
                     </p>
                  </div>
             </div>
         </div>
       </div>
-      <div className="bg-zinc-50" id="artistas">
+      {/* <div className="bg-zinc-50" id="artistas">
         <div className="flex pt-12">
             <div className="w-2/5">
                 <div className="container pt-6 pl-24 pr-10">
@@ -70,9 +88,160 @@ const ArteInformacion = () => {
                  </div>
             </div>
         </div>
-      </div>
+      </div> */}
       <div className="bg-zinc-50" id="mapa-arte">
-        <img src="/static/images/arte.png"  className="h-1/2 pb-5 pt-16" alt="" />
+        <img src="/static/images/arte.png"  className="h-1/2 pt-16" alt="" />
+      </div>
+      <div className="bg-neutral-500 pt-10" id="listas">
+        <div className="flex ">
+        <div className="w-3/5 text-neutral-500 pt-6 text-left pr-16 ml-16">
+                 <div className="container text-white ">
+                    <h2 className="pb-7 subtitle-section">Murales figurativos cerámicos y al fresco</h2>
+                    <ul>
+                      <li>i.1.3.1 (Fechas Magnas de la Universidad)</li>
+                      <li>i.1.3.2 (Hecto Poleo en Rectorado)</li>
+                      <li>i.2.1.1 (Instituto Medicina Experimental)</li>
+                      <li>ii.2.2.1 (Instituto Anatómico)</li>
+                      <li>ii.5.1.1 (mural de la Capilla)</li>
+                      <li>iv.2.1.1 (mural del Comedor)</li>
+                    </ul>
+                 </div>
+                 <div className="container text-white pt-7">
+                    <h2 className="pb-5 subtitle-section">Esculturas y Bajorrelieve figurativos</h2>
+                    <ul>
+                      <li>ii.2.1.3 (La Educación)</li>
+                      <li>ii.2.2.2 (La Ciencia)</li>
+                      <li>vi.1.1.16 (Torso, FAU)</li>
+                      <li>vii.1.1.7 (el atleta)</li>
+                      <li>ii.2.1.2 Dr. José Gregorio Hernández</li>
+                    </ul>
+                 </div>
+                 <div className="container text-white pt-7">
+                    <h2 className="pb-7 subtitle-section">Bimurales abstractos</h2>
+                    <ul>
+                      <li>i.2.1.3 (Léger junto al Amphion)</li>
+                      <li>i.2.1.4 (Mateo Manaure junto al Pastor de Nubres)</li>
+                      <li>i.2.1.5 (Homenaje a Malevitch)</li>
+                    </ul>
+                 </div>
+                 <div className="container text-white pt-6 ">
+                    <h2 className="pb-7 subtitle-section">Bajorrelieves abstractos</h2>
+                    <ul>
+                      <li>i.1.4.2 (Andre Bloc en Escuela de Comunicaciones)</li>
+                      <li>i.2.4.1 Elementos acústicos de la Sala de Conciertos</li>
+                      <li>i.2.5.3 Progresión rítmica (último piso Biblioteca Central)</li>
+                      <li>i.2.5.4 Progresión rítmica (último piso Biblioteca Central</li>
+                      <li>iii.1.1.1 (Siluetas en relieves, Biblioteca de Psicología)</li>
+                      <li>v.1.1.1 (Wilfredo Lam en Instituto Botánico)</li>
+                      <li>v.1.1.2 (Narváez en Instituto Botánico)</li>
+                      <li>vi.1.1.1 (Taller Galia)</li>
+                      <li>vi.1.1.2 (Taller Galia)</li>
+                      <li>vi.1.1.3 (Taller Galia)</li>
+                      <li>vi.1.1.6 (Taller Galia)</li>
+                      <li>vi.1.1.7 (Alirio Oramas, Centro de Estudiantes FAU)</li>
+                      <li>viii.1.1.1</li>
+                    </ul>
+                 </div>
+                 <div className="container text-white pt-7">
+                    <h2 className="pb-7 subtitle-section">Esculturas abstractos</h2>
+                    <ul>
+                      <li>i.1.1.1 (La Cultura)</li>
+                      <li>i.2.1.1 (Pastor de Nubes)</li>
+                      <li>i.2.1.2 (Amphion)</li>
+                      <li>i.2.2.1 (Nubes Acústicas de Calder)</li>
+                      <li>i.2.4.4 (Positivo-Negativo)</li>
+                      <li>i.2.6.1 (Dinamismo en 30 grados)</li>
+                      <li>i.3.1.1 (La Maternidad)</li>
+                      <li>vi.1.1.7 (Ráfaga de nieve)</li>
+                      <li>vi.1.1.8 (Móbile)</li>
+                      <li>vi.1.1.9 (Stabile Móbile)</li>
+                    </ul>
+                 </div>
+                 <div className="container text-white pt-7">
+                    <h2 className="pb-5 subtitle-section">Vitrales abstractos</h2>
+                    <ul>
+                      <li>i.2.3.2 (Paraninfo)</li>
+                      <li>i.2.3.3 (Paraninfo)</li>
+                      <li>i.2.5.7 (Léger de Biblioteca) (Torso, FAU)</li>
+                      <li>ii.7.2.1 (Escuela de Enfermería) (el atleta)</li>
+                      <li>iii.3.2.1 (Biblioteca de Ingeniería)</li>
+                    </ul>
+                 </div>
+                 <div className="container text-white pt-7">
+                    <h2 className="pb-5 subtitle-section">Cientismo</h2>
+                    <ul>
+                      <li>vi.1.1.19 (Jesús Soto de la FAU)</li>
+                    </ul>
+                 </div>
+            </div>
+            <div className="w-3/5 text-neutral-500 pt-6 text-left pr-16 ml-16">
+            <div className="container text-white ">
+                    <h2 className="pb-7 subtitle-section">Murales abstractos</h2>
+                    <ul>
+                      <li>I.1.2.1</li>
+                      <li>I.1.2.2</li>
+                      <li>i.1.3.3</li>
+                      <li>i.1.3.4</li>
+                      <li>i.1.4.1</li>
+                      <li>i.2.1.6</li>
+                      <li>i.2.2.2</li>
+                      <li>i.2.2.3</li>
+                      <li>i.2.3.1</li>
+                      <li>i.2.4.2</li>
+                      <li>i.2.4.3</li>
+                      <li>i.2.5.1</li>
+                      <li>i.2.5.2</li>
+                      <li>i.2.5.5</li>
+                      <li>i.2.5.6</li>
+                      <li>i.2.6.2</li>
+                      <li>ii.1.1.2</li>
+                      <li>ii.1.1.3</li>
+                      <li>ii.1.1.4</li>
+                      <li>ii.1.1.5</li>
+                      <li>ii.1.1.6</li>
+                      <li>ii.1.1.7</li>
+                      <li>ii.1.1.8</li>
+                      <li>iii.1.1.2</li>
+                      <li>iii.1.1.3</li>
+                      <li>iii.1.1.4</li>
+                      <li>iii.1.1.5</li>
+                      <li>iii.1.1.6</li>
+                      <li>iii.1.1.7</li>
+                      <li>iii.1.1.8</li>
+                      <li>iii.1.1.9</li>
+                      <li>iii.1.1.10</li>
+                      <li>iii.1.1.11</li>
+                      <li>iii.1.1.12</li>
+                      <li>iii.1.1.13</li>
+                      <li>iii.1.1.14</li>
+                      <li>iii.1.1.15</li>
+                      <li>iii.1.1.16</li>
+                      <li>iii.3.1.1</li>
+                      <li>iii.3.1.2</li>
+                      <li>iii.3.1.3</li>
+                      <li>iii.3.1.4</li>
+                      <li>iii.3.2.2</li>
+                      <li>iii.3.2.3</li>
+                      <li>vi.1.1.4</li>
+                      <li>vi.1.1.6</li>
+                      <li>vi.1.1.10</li>
+                      <li>vi.1.1.11</li>
+                      <li>vi.1.1.12</li>
+                      <li>vi.1.1.13</li>
+                      <li>vi.1.1.14</li>
+                      <li>vi.1.1.15</li>
+                      <li>vi.1.1.20</li>
+                      <li>vi.1.1.21</li>
+                      <li>vii.1.1.1</li>
+                      <li>vii.1.1.2</li>
+                      <li>vii.1.1.3</li>
+                      <li>vii.1.1.4</li>
+                      <li>vii.1.1.5</li>
+                      <li>vii.1.1.6</li>
+                    </ul>
+                 </div>
+            </div>
+        </div>
       </div>
     </div>
   );
